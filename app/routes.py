bc6572cdedb90e50a081fdadd8f2442d3e47c29d
Mike <michaelkeohane01@gmail.com>
2024-07-14 20:17:53 -0400

    Created the HTML frontend

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,20 @@
-from flask import Flask, render_template
+from flask import Flask, request, jsonify, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/ask', methods=['POST'])
+def ask():
+    data = request.get_json()
+    question = data.get('question')
+    print(data)
+    # Use your LLM pipeline here to generate the response
+    answer = "This is a placeholder answer to your question: " + question
+
+    return jsonify({'answer': answer})
+
+if __name__ == '__main__':
+    app.run(debug=True)
