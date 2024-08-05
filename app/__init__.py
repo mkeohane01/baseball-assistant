8c8c4d460c39a2af78abc2bc754762328d70138a
Mike Keohane <michael.keohane@duke.edu>
2024-08-05 03:54:39 +0000

    Fixed lint

--- app/__init__.py
+++ app/__init__.py
@@ -3,18 +3,18 @@ from .routes import app as routes_blueprint
 from src.populate_db import update_pitching_stats, update_hitting_stats
 
 def create_app():
-    app = Flask(__name__)
+    baseball_app = Flask(__name__)
 
     # Register blueprints
-    app.register_blueprint(routes_blueprint)
+    baseball_app.register_blueprint(routes_blueprint)
 
     # Populate the database when the app is created
     update_pitching_stats()
     update_hitting_stats()
 
-    return app
+    return baseball_app
 
 #if you want to run the app directly from this file
 if __name__ == "__main__":
-    app = create_app()
-    app.run()
+    baseball_app = create_app()
+    baseball_app.run()
