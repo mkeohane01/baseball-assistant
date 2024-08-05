ed872abf0b01bd9fe57d0888635785502c28c793
Mike <michaelkeohane01@gmail.com>
2024-08-04 22:34:23 -0400

    Built extraction pipeline

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,7 @@
 from .routes import app
+from src.populate_db import update_pitching_stats, update_hitting_stats
 
 def main():
+    update_pitching_stats()
+    update_hitting_stats()
     app.run()
